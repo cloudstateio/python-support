f70ad028ba509956290b2af5f8f19716de85109c
Adriano Santos <sleipnir@bsd.com.br>
2020-07-16 15:12:47 -0300

    Feature/fluent configuration api (#39)
    * Add support to UDS and more fluent Cloudstate API
    
    * Use private members and some docstrings

--- cloudstate/cloudstate.py
+++ cloudstate/cloudstate.py
@@ -2,6 +2,7 @@
 Copyright 2020 Lightbend Inc.
 Licensed under the Apache License, Version 2.0.
 """
+from typing import Optional
 
 from dataclasses import (dataclass, field)
 from typing import List
@@ -14,25 +15,53 @@ from cloudstate.evensourced_servicer import CloudStateEventSourcedServicer
 from cloudstate.event_sourced_entity import EventSourcedEntity
 from cloudstate.discovery_servicer import CloudStateEntityDiscoveryServicer
 from cloudstate.entity_pb2_grpc import add_EntityDiscoveryServicer_to_server
-from pprint import pprint
+
+import logging
 
 from cloudstate.event_sourced_pb2_grpc import add_EventSourcedServicer_to_server
 
-
 @dataclass
 class CloudState:
-    event_sourced_entities: List[EventSourcedEntity] = field(default_factory=list)
+    logging.basicConfig(format='%(asctime)s - %(filename)s - %(levelname)s: %(message)s', level=logging.INFO)
+    logging.root.setLevel(logging.NOTSET)
+
+    __host = '127.0.0.1'
+    __port = '8080'
+    __workers = 10
+    __event_sourced_entities: List[EventSourcedEntity] = field(default_factory=list)
+
+    def host(self, address: str):
+        """Set the Network Host address."""
+        self.__host = address
+        return self
+
+    def port(self, port: str):
+        """Set the Network Port address."""
+        self.__port = port
+        return self
+
+    def max_workers(self, workers: Optional[int] = 10):
+        """Set the gRPC Server number of Workers."""
+        self.__workers = workers
+        return self
 
     def register_event_sourced_entity(self, entity: EventSourcedEntity):
-        self.event_sourced_entities.append(entity)
+        """Registry the user EventSourced entity."""
+        self.__event_sourced_entities.append(entity)
         return self
 
     def start(self):
-        server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-        add_EntityDiscoveryServicer_to_server(CloudStateEntityDiscoveryServicer(self.event_sourced_entities), server)
-        add_EventSourcedServicer_to_server(CloudStateEventSourcedServicer(self.event_sourced_entities), server)
-        port = os.environ.get('HOST', '127.0.0.1') + ':' + os.environ.get('PORT', '8080')
-        server.add_insecure_port(port)
-        pprint('Starting Cloudstate on ' + port)
-        server.start()
+        """Start the user function and gRPC Server."""
+        address = '{}:{}'.format(os.environ.get('HOST', self.__host), os.environ.get('PORT', self.__port))
+        server = grpc.server(futures.ThreadPoolExecutor(max_workers=self.__workers))
+        add_EntityDiscoveryServicer_to_server(CloudStateEntityDiscoveryServicer(self.__event_sourced_entities), server)
+        add_EventSourcedServicer_to_server(CloudStateEventSourcedServicer(self.__event_sourced_entities), server)
+
+        logging.info('Starting Cloudstate on address %s', address)
+        try:
+            server.add_insecure_port(address)
+            server.start()
+        except IOError as e:
+            logging.error('Error on start Cloudstate %s', e.__cause__)
+
         server.wait_for_termination()
--- shoppingcart/shopping_cart.py
+++ shoppingcart/shopping_cart.py
@@ -5,8 +5,9 @@ Licensed under the Apache License, Version 2.0.
 
 from cloudstate.cloudstate import CloudState
 from shoppingcart.shopping_cart_entity import entity as shopping_cart_entity
-import logging
 
 if __name__ == '__main__':
-    logging.basicConfig()
-    CloudState().register_event_sourced_entity(shopping_cart_entity).start()
+    CloudState()\
+        .port('8080')\
+        .register_event_sourced_entity(shopping_cart_entity)\
+        .start()
