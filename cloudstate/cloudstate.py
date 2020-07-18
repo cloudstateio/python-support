c35175f93cb863c3b513ed827a1e0c9e6d4d0d16
Adriano Santos <sleipnir@bsd.com.br>
2020-07-18 05:46:39 -0300

    Feature/better defaults explanations (#40)
    * Add support to UDS and more fluent Cloudstate API
    
    * Use private members and some docstrings
    
    * Some documentation about defaults
    
    * Api adjusts
    
    * Update cloudstate/cloudstate.py
    
    Co-authored-by: Marcel Lanz <marcellanz@n-1.ch>
    
    * Update cloudstate/cloudstate.py
    
    Co-authored-by: Marcel Lanz <marcellanz@n-1.ch>
    
    Co-authored-by: Marcel Lanz <marcellanz@n-1.ch>

--- cloudstate/cloudstate.py
+++ cloudstate/cloudstate.py
@@ -17,6 +17,7 @@ from cloudstate.discovery_servicer import CloudStateEntityDiscoveryServicer
 from cloudstate.entity_pb2_grpc import add_EntityDiscoveryServicer_to_server
 
 import logging
+import multiprocessing
 
 from cloudstate.event_sourced_pb2_grpc import add_EventSourcedServicer_to_server
 
@@ -25,23 +26,30 @@ class CloudState:
     logging.basicConfig(format='%(asctime)s - %(filename)s - %(levelname)s: %(message)s', level=logging.INFO)
     logging.root.setLevel(logging.NOTSET)
 
+    __address: str = ''
     __host = '127.0.0.1'
     __port = '8080'
-    __workers = 10
+    __workers = multiprocessing.cpu_count()
     __event_sourced_entities: List[EventSourcedEntity] = field(default_factory=list)
 
     def host(self, address: str):
-        """Set the Network Host address."""
+        """Set the address of the network Host.
+           Default Address is 127.0.0.1.
+        """
         self.__host = address
         return self
 
     def port(self, port: str):
-        """Set the Network Port address."""
+        """Set the address of the network Port.
+        Default Port is 8080.
+        """
         self.__port = port
         return self
 
-    def max_workers(self, workers: Optional[int] = 10):
-        """Set the gRPC Server number of Workers."""
+    def max_workers(self, workers: Optional[int] = multiprocessing.cpu_count()):
+        """Set the gRPC Server number of Workers.
+           Default is equal than number of CPU Cores in the machine.
+        """
         self.__workers = workers
         return self
 
@@ -52,14 +60,16 @@ class CloudState:
 
     def start(self):
         """Start the user function and gRPC Server."""
-        address = '{}:{}'.format(os.environ.get('HOST', self.__host), os.environ.get('PORT', self.__port))
+
+        self.__address = '{}:{}'.format(os.environ.get('HOST', self.__host), os.environ.get('PORT', self.__port))
+
         server = grpc.server(futures.ThreadPoolExecutor(max_workers=self.__workers))
         add_EntityDiscoveryServicer_to_server(CloudStateEntityDiscoveryServicer(self.__event_sourced_entities), server)
         add_EventSourcedServicer_to_server(CloudStateEventSourcedServicer(self.__event_sourced_entities), server)
 
-        logging.info('Starting Cloudstate on address %s', address)
+        logging.info('Starting Cloudstate on address %s', self.__address)
         try:
-            server.add_insecure_port(address)
+            server.add_insecure_port(self.__address)
             server.start()
         except IOError as e:
             logging.error('Error on start Cloudstate %s', e.__cause__)
--- shoppingcart/shopping_cart.py
+++ shoppingcart/shopping_cart.py
@@ -8,6 +8,6 @@ from shoppingcart.shopping_cart_entity import entity as shopping_cart_entity
 
 if __name__ == '__main__':
     CloudState()\
-        .port('8080')\
+        .port('8090')\
         .register_event_sourced_entity(shopping_cart_entity)\
         .start()
